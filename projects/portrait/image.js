cea73b56461a3f528f1411501ce820d733ddea0c
CoalCats1 <Cole.Dom.Carney@gmail.com>
2026-02-10 13:34:05 -0500

    new project self portrate

---
+++ projects/portrait/image.js
@@ -0,0 +1,46 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+//background
+
+//face
+ctx.fillStyle = "Peach";
+ctx.beginPath();
+ctx.ellipse(
+	400,300,
+	150,
+	200,
+	0,
+	0,
+	2*Math.PI
+);
+ctx.fill();
+//hair
+
+//ears
+
+//eye whites
+
+//pupile
+
+//irs
+
+//smile
+
+//nose
+
+//arm laft
+
+//arm right
+
+//hand left 
+
+//hand right
+
+//shirt
+
+//neck
+
+//10 fingers
+
+
