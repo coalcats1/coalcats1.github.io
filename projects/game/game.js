d7dbcccb7aeb5866e888195660a419832204f1e8
CoalCats1 <Cole.Dom.Carney@gmail.com>
2026-03-05 12:50:50 -0500

    this thing

--- projects/game/game.js
+++ projects/game/game.js
@@ -2,41 +2,43 @@ let gameActive = true; //this variable is required.
                        //to stop the game, set it to false.
 
 //Declare your other global variables here
-
+let t = t+1
 
 //If you need, add any "helper" functions here
-
+if(t < 16815){
+	gameActive = false;
+}
 
 //Make one function for each location
-function locationA() {
+function Void() {
     clear();
-    print("\nYou are in location A!");
+    print("\nYou are in the Void!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationB");
+        "\n\tcult meeting");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationb") {
-            locationB();
+        if (input.toLowerCase() === "cult meeting") {
+            cult_meeting();
         } else {
             stayHere();
-            waitThenCall(locationA);
+            waitThenCall(Void);
         }
     }
     waitForInput(processInput);
 }
 
-function locationB() {
+function cult_meeting() {
     clear();
-    print("\nYou are in location B!");
+    print("\nYou are in cult meeting!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationA");
+        "\n\tVoid");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationa") {
+        if (input.toLowerCase() === "Void") {
             locationA();
         } else {
             stayHere();
-            waitThenCall(locationB);
+            waitThenCall(cult_meeting);
         }
     }
     waitForInput(processInput);
@@ -49,7 +51,7 @@ function start(){
     print("Welcome to my game! Press any key to start");
 
     function processInput(input){
-            locationA();
+            Void();
     }
     waitForInput(processInput);
 }
