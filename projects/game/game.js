456a0fb49204b527414104ebda326da612214daa
CoalCats1 <Cole.Dom.Carney@gmail.com>
2026-03-05 18:24:18 -0500

    new room

--- projects/game/game.js
+++ projects/game/game.js
@@ -2,12 +2,13 @@ let gameActive = true; //this variable is required.
                        //to stop the game, set it to false.
 
 //Declare your other global variables here
-let t = t+1
 
 //If you need, add any "helper" functions here
-if(t < 16815){
+setTimeout(() => {
+	clear();
+	print("\nYou Have Become");
 	gameActive = false;
-}
+}, 7 * 60 * 1000)  
 
 //Make one function for each location
 function Void() {
@@ -33,18 +34,39 @@ function cult_meeting() {
     print("\nHooded figures huttle around a central ulter. One of them gestures at you and says,");
     print("\nHey were you been, were doing it! Were gonna contact the them!");
     print("\nYou stare blankly at the hooded face hidden in shadows.");
-    print("\nYou have the urge to move, something is eating at you. Where do you want to go next? Say one of these choices:" +
-        "\n\tVoid");
+    print("\nYou have the urge to move, something is eating at you. You see a shard of stained glass do you enter. Where do you want to go next? Say one of these choices:" +
+        "\n\tVoid"+"\n\tEnd Times");
     
     function processInput(input){
-        if (input.toLowerCase() === "Void") {
-            locationA();
-        } else {
+        if (input.toLowerCase() === "void") {
+            Void();
+        }else if(input.toLowerCase() === "end times"){
+		End_Times();
+	}else{
             stayHere();
             waitThenCall(cult_meeting);
         }
     }
     waitForInput(processInput);
+}
+function End_Times(){
+	clear();
+	print("\nThe end of the world is upon you.");
+
+	print("\nYou know you must keep moving, You know you must keep moving. Where to next? Say one of these choices:" +
+		"\n\tVoid"+"\n\tcult meeting");
+
+	function processInput(input){
+		if(input.toLowerCase() === "void"){
+			Void();
+		}else if(input.toLowerCase() === "cult meeting"){
+			cult_meeting();
+		}else{
+			stayHere();
+			waitThenCall(End_Times);
+		}
+	}
+	waitForInput(processInput);
 }
 
 //finally, make sure you customize this to tell it what should happen at the
