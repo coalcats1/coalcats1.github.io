4b6a9604d5dfa0bf42cbf4a3d7e4cbdbd038d4c5
CoalCats1 <Cole.Dom.Carney@gmail.com>
2026-03-06 12:55:27 -0500

    two new rooms added

--- projects/game/game.js
+++ projects/game/game.js
@@ -15,12 +15,14 @@ function Void() {
     clear();
     print("\nYou are in the Void!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tcult meeting");
+        "\n\tcult meeting"+"\n\tStorm");
     
     function processInput(input){
         if (input.toLowerCase() === "cult meeting") {
             cult_meeting();
-        } else {
+        }else if(input.toLowerCase() === "storm"){
+                storm();
+	}else{
             stayHere();
             waitThenCall(Void);
         }
@@ -34,7 +36,7 @@ function cult_meeting() {
     print("\nHooded figures huttle around a central ulter. One of them gestures at you and says,");
     print("\nHey were you been, were doing it! Were gonna contact the them!");
     print("\nYou stare blankly at the hooded face hidden in shadows.");
-    print("\nYou have the urge to move, something is eating at you. You see a shard of stained glass do you enter. Where do you want to go next? Say one of these choices:" +
+    print("\nYou have the urge to move, something is eating at you. You see a shard of stained glass do you enter? Where do you want to go next? Say one of these choices:" +
         "\n\tVoid"+"\n\tEnd Times");
     
     function processInput(input){
@@ -52,7 +54,8 @@ function cult_meeting() {
 function End_Times(){
 	clear();
 	print("\nThe end of the world is upon you.");
-
+	print("\nIt is as you expect, meteors rain upon the world and, in the distance a wall of fire sweeps across the land turning rock into lava.");
+	print("\nThe heat causes something to glimmer, a shard of glass.");
 	print("\nYou know you must keep moving, You know you must keep moving. Where to next? Say one of these choices:" +
 		"\n\tVoid"+"\n\tcult meeting");
 
@@ -64,6 +67,44 @@ function End_Times(){
 		}else{
 			stayHere();
 			waitThenCall(End_Times);
+		}
+	}
+	waitForInput(processInput);
+}
+function storm(){
+	clear();
+	print("\nYou're on a hill, a massive thunder strom covers the shy over head.");
+	print("\nYou turn around trying to find your barings. You see someone in the distance running and waving at you.");
+	print("\nLightning splits the sky overhead, you don't notice your temporary hearing loss. The lightning alluminated something in the ground, a glass shard");
+	print("\nYou need to get moving, something is happening, but you can't put your finger on it. Where to next?" +
+		"\n\tVoid"+"\n\tThe Lake of Sorrow");
+	function processInput(input){
+		if(input.toLowerCase() === "void"){
+			Void();
+		}else if(input.toLowerCase() === "the lake of sorrow"){
+			sorrow();
+		}else{
+			stayHere();
+			waitThenCall(storm);
+		}
+	}
+	waitForInput(processInput);
+}
+function sorrow(){
+	clear();
+	print("\nYou look over a perfectly still lake, you wade into the water, only to find that it is too smooth.");
+	print("\nSomething bubles up inside you an immense regret.");
+	print("\nA large disk rests in the liquide, you need to go, keep moving. There is nothing here for you, not anymore.");
+	print("\nWhere do you go now?" + 
+		"\n\tVoid"+"\n\tStorm");
+	function processInput(input){
+		if(input.toLowerCase() === "storm"){
+			storm();
+		}else if(input.toLowerCase() === "void"){
+			Void();
+		}else{
+			stayHere();
+			waitThenCall(sorrow);
 		}
 	}
 	waitForInput(processInput);
